0050194373264a00e6898a95e3ae100d6bb2e587
JimRSA <132478070+JimRSA@users.noreply.github.com>
2023-08-09 10:42:38 +0200

    Update build_hpl.sh

--- build_hpl.sh
+++ build_hpl.sh
@@ -32,22 +32,19 @@ else
 fi
 
 
-# Step 2: Download HPL
-HPL_SOURCE_URL="https://www.netlib.org/benchmark/hpl/hpl-2.3.tar.gz"
-wget $HPL_SOURCE_URL
-tar -xvf hpl-2.3.tar.gz
-cd hpl-2.3
-
-
+if ! command -v openmpi &> /dev/null
+then
+    echo "wget not found, installing..."
+##INSTALL OPENMPI v4.1.5
 # Get the installation directory from the user
 read -p "Enter the installation directory for OpenMPI: " INSTALL_DIR
 
 # Download and extract OpenMPI
-wget https://download.open-mpi.org/release/open-mpi/v4.1/openmpi-4.1.3.tar.gz
-tar -xzf openmpi-4.1.3.tar.gz
+wget https://download.open-mpi.org/release/open-mpi/v4.1/openmpi-4.1.5.tar.gz
+tar -xzf openmpi-4.1.5.tar.gz
 
 # Configure and install OpenMPI
-cd openmpi-4.1.3
+cd openmpi-4.1.5
 ./configure --prefix=$INSTALL_DIR
 make -j$(nproc)
 make install
@@ -56,3 +53,55 @@ make install
 mkdir -p $INSTALL_DIR/modules
 cat << EOF > $INSTALL_DIR/modules/openmpi
 #%Module1.0
+prepend-path PATH $INSTALL_DIR/bin
+prepend-path LD_LIBRARY_PATH $INSTALL_DIR/lib
+EOF
+
+# Append the moduleuse line to the bashrc
+echo "module use --append $INSTALL_DIR/modules" >> ~/.bashrc
+source ~/.bashrc
+##END OF OPENMPI INSTALL
+
+
+
+##INSTALL OPENBLAS v0.3.23
+read -p "Enter the installation directory for OpenBLAS: " INSTALL_DIR
+
+# Configure and install OpenBLAS
+cd OpenBLAS-0.3.23
+make FC=gfortran
+
+# Write the module file
+mkdir -p $INSTALL_DIR/modules
+cat << EOF > $INSTALL_DIR/modules/openBLAS
+#%Module1.0
+prepend-path PATH $INSTALL_DIR/bin
+prepend-path LD_LIBRARY_PATH $INSTALL_DIR/lib
+EOF
+
+make $INSTALL_DIR/OpenBLAS-0.3.23 install
+
+# Append the moduleuse line to the bashrc
+echo "module use --append $INSTALL_DIR/modules" >> ~/.bashrc
+source ~/.bashrc
+##END OF OPENBLAS INSTALL
+
+
+##INSTALL HPL-2.3
+# Get the installation directory from the user
+read -p "Enter the installation directory for HPL: " INSTALL_DIR
+
+# Download and extract HPL
+wget https://www.netlib.org/benchmark/hpl/hpl-2.3.tar.gz
+tar -xzf hpl-2.3.tar.gz
+
+# Configure and copy setup to working dir
+cd hpl-2.3
+cp setup/Linux_PII_CBLAS_gm .
+
+##<File setup here>##
+
+# Build HPL
+make arch=Linux_PII_CBLAS_gm
+##END OF HPL INSTALL
+
